852e62d847906a3038c083d50ff5de87e9daa069
Örsan <guenay@gmail.com>
2025-03-25 22:58:51 +0100

    Initial Mitosis Config

---
+++ test-apps/react/src/App.jsx
@@ -0,0 +1,13 @@
+import { AutoComplete, Todos } from '@mitosis-sandbox/library-react';
+
+function App() {
+  return (
+    <div>
+      <div>Welcome to React.</div>
+      <AutoComplete />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
